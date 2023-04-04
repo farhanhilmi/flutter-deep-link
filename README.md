a0ac8ae3ba2802a4c8c388915d472712378fc0ca
Farhan Hilmi <farhanhilmi077@gmail.com>
2023-04-04 17:50:02 +0700

    firebase dynamic link

--- README.md
+++ README.md
@@ -1,16 +1,55 @@
-# test_project
+# Instruction Flutter App Link
 
-A new Flutter project.
+Official documentation about flutter `firebase_core` and `firebase_dynamic_links` :
 
-## Getting Started
+-   https://pub.dev/packages/firebase_core
+-   https://pub.dev/packages/firebase_dynamic_links
 
-This project is a starting point for a Flutter application.
+### Install package
 
-A few resources to get you started if this is your first Flutter project:
+```
+- flutter pub add firebase_dynamic_links
+- flutter pub add firebase_core
+```
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+### Update lib/main.dart
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+inside main function
+
+```
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+```
+
+---
+
+<br><br>
+
+# In Server
+
+For more information: https://simonmarquis.github.io/Android-App-Linking/app-links/
+
+### Create well-known directory in server root
+
+-   create `assetlinks.json` file
+-   store this code in that file
+    ```
+    [
+      {
+          "relation": [
+              "delegate_permission/common.handle_all_urls"
+          ],
+          "target": {
+              "namespace": "android_app",
+              "package_name": "com.example.test_project",
+              "sha256_cert_fingerprints": [
+                  "6B:41:26:A6:1E:D8:BD:91:D3:8B:57:10:5F:07:5C:2D:AB:3E:26:A4:D1:3C:9C:97:15:78:9E:0D:56:0A:CE:DC"
+              ]
+          }
+      }
+    ]
+    ```
+-   To get cert fingerprints, type `gradlew signingReport` inside `android` folder
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -48,68 +52,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  Future initDynamicLinks() async {
+    dynamicLinks.onLink.listen((event) {
+      // navigator ke halaman forget password
+      var uid = event.link.queryParameters['uid'];
+      var token = event.link.queryParameters['token'];
+      print('dynmic link: ${event.link}');
+      print('userId: ' + uid.toString());
+      print('token: ' + token.toString());
+    }).onError((error) {
+      print('error ${error.message}');
     });
   }
 
   @override
+  void initState() {
+    super.initState();
+    initDynamicLinks();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Container(),
     );
   }
 }
